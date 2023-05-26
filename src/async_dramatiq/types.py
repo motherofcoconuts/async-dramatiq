22be135aeb2eb512a694682f5827e0fb38c036e8
Ryan Houlihan <ryan@batontrucking.com>
2023-05-25 18:09:25 -0700

    Fix adding types

---
+++ src/async_dramatiq/types.py
@@ -0,0 +1,17 @@
+# Standard Library Imports
+from dataclasses import dataclass
+from enum import IntEnum
+
+class DramatiqWorkerPriority(IntEnum):
+    CRITICAL = 0
+    VERY_HIGH = 1
+    HIGH = 3
+    MEDIUM = 4
+    LOW = 5
+    VERY_LOW = 6
+
+
+@dataclass
+class TaskQueue:
+    queue: str
+    message_ttl: int | None = 60 * 60  # Max time a task can sit in the queue

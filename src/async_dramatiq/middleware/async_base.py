b61816a4052d17f130276b94bf42dcae0b89f5fc
Aramayis Orkusyan <aramayis@baton.io>
2024-05-09 16:34:02 -0700

    Make AsyncMiddleware.before_worker_boot callable only once

--- src/async_dramatiq/middleware/async_base.py
+++ src/async_dramatiq/middleware/async_base.py
@@ -1,4 +1,5 @@
 # Standard Library Imports
+import threading
 from threading import Event
 from typing import Any
 
@@ -7,16 +8,29 @@ import dramatiq as dq
 # Local Application Imports
 from async_dramatiq.worker import AsyncWorker
 
+lock = threading.Lock()
+is_initialized = False
+
 
 class AsyncMiddleware(dq.Middleware):
     """Middleware that runs the Asyncio Event Loop in a separate worker."""
 
     def before_worker_boot(self, broker: dq.Broker, worker: dq.Worker) -> None:
-        startup_event = Event()
-        async_worker = AsyncWorker(startup_event, broker)
-        async_worker.start()
-        startup_event.wait()  # Wait until the Asyncio Event Loop has started
-        worker.workers.append(async_worker)
+        global lock, is_initialized
+
+        if lock.acquire(blocking=False):
+            if not is_initialized:
+                startup_event = Event()
+                async_worker = AsyncWorker(startup_event, broker)
+                async_worker.start()
+                startup_event.wait()  # Wait until the Asyncio Event Loop has started
+                worker.workers.append(async_worker)
+                is_initialized = True
+            else:
+                raise RuntimeError(
+                    "AsyncWorker already initialized with an event loop."
+                )
+            lock.release()
 
     def before_async_worker_thread_startup(
         self, broker: dq.Broker, thread: AsyncWorker, **kwargs: dict[str, Any]  # noqa
